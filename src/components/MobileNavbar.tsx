f04fb4cd23ddc68de1c881595c6dfd1dc6ecbc09
blazrr <selmhamedi@gmail.com>
2023-01-16 22:31:03 +0100

    Started mobile navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -4,32 +4,36 @@ type Props = {}
 
 const MobileNavbar = (props: Props) => {
     return (
-        <div className='flex flex-col md:flex-row md:space-x-8 md w:4/5 md:p-8'>
+        <div className='flex flex-col md:flex-row md:space-x-8 pt-14  w:4/5 md:p-14 space-y-16 md:space-y-0 bg-red-400 '>
 
+            
+            <div className='navCard'>
 
-            <div className='flex flex-col w-2/5 md:w-auto mx-auto justify-center items-center space-y-4 border-2 border-red '>
-                
-                <div className='flex'>
-                    <img src="https://m.media-amazon.com/images/I/71+Uwt-oIgL._AC_SL1500_.jpg" alt="" className='' />
+                <div className='absolute  drop-shadow-2xl top-[-25%] w-[70%] max-w-[230px]'>
+                    <img src="https://cdn.pixabay.com/photo/2015/10/01/17/17/car-967387__480.png" alt="" className='' />
                 </div>
-                <span>Headphones</span>
-                <button>Shop</button>
+                <span className='font-semibold tracking-wide '>Headphones</span>
+                <button className='tracking-wider font-semibold text-[#979797] '>Shop</button>
             </div>
 
-            <div className='flex flex-col w-2/5 md:w-auto mx-auto justify-center items-center space-y-4 border-2 border-red'>
-                <div>
-                    <img src="https://m.media-amazon.com/images/I/71+Uwt-oIgL._AC_SL1500_.jpg" alt="" className='filter drop-shadow-2xl"'/>
+
+            <div className='navCard'>
+
+                <div className='absolute  drop-shadow-2xl top-[-25%] w-[70%] max-w-[230px]'>
+                    <img src="https://cdn.pixabay.com/photo/2015/10/01/17/17/car-967387__480.png" alt="" className='' />
                 </div>
-                <span>Headphones</span>
-                <button>Shop</button>
+                <span className='font-semibold tracking-wide'>Headphones</span>
+                <button className=' font-semibold text-[#979797] tracking-wider	 '>Shop</button>
             </div>
 
-            <div className='flex flex-col w-2/5 md:w-auto mx-auto justify-center items-center space-y-4 border-2 border-red'>
-                <div>
-                    <img src="https://m.media-amazon.com/images/I/71+Uwt-oIgL._AC_SL1500_.jpg" alt="" className='filter drop-shadow-[0_320px_320px_rgba(0,0,0,0.5)]' />
+
+            <div className='navCard'>
+
+                <div className='absolute  drop-shadow-2xl top-[-25%] w-[70%] max-w-[230px]'>
+                    <img src="https://cdn.pixabay.com/photo/2015/10/01/17/17/car-967387__480.png" alt="" className='' />
                 </div>
-                <span>Headphones</span>
-                <button>Shop</button>
+                <span className='font-semibold tracking-wide'>Headphones</span>
+                <button className='tracking-wider font-semibold text-[#979797] '>Shop</button>
             </div>
 
 
